fc2df404484e5cce5a3f39c116b75d26e572427c
Hossein Ansari <hosseinansari3@gmail.com>
2023-05-27 22:21:38 +0430

    changing baseurl

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:5000";
+const baseUrl = "https://bikershop2.onrender.com";
 
 export const signIn = (email, password) =>
   axios.post(`${baseUrl}/users/signin`, { email, password });
