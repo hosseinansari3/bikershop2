c0f48ff8857b97aa754802683da0d013db7b2171
Hossein Ansari <hosseinansari3@gmail.com>
2023-05-30 10:49:35 +0430

    fghjfghjg

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -6,52 +6,54 @@ import { LocationOn, Email, LocalPhone } from "@mui/icons-material";
 
 function Footer() {
   return (
-    <div className="Footer grid grid-cols-4">
-      <div className="footer-col col-span-2">
-        <div>
-          <h4>BIKERSHOP</h4>
+    <div className="Footer p-5">
+      <div>
+        <h4>BIKERSHOP</h4>
+      </div>
+      <div className=" grid grid-cols-4">
+        <div className="p-5 col-span-4 md:col-span-2">
+          <p>
+            Outstock is a premium Templates theme with advanced admin module.
+            It’s extremely customizable, easy to use and fully responsive and
+            retina ready.
+          </p>
+          <div>
+            <ul>
+              <li>
+                <LocationOn /> Add: 1234 Heaven Stress, Beverly Hill, Melbourne,
+                USA.
+              </li>
+              <li>
+                <Email /> Email: Contact@basictheme.com
+              </li>
+              <li>
+                <LocalPhone /> Phone Number: (800) 123 456 789
+              </li>
+            </ul>
+          </div>
         </div>
-        <p>
-          Outstock is a premium Templates theme with advanced admin module. It’s
-          extremely customizable, easy to use and fully responsive and retina
-          ready.
-        </p>
-        <div>
+
+        <div className="p-5 col-span-2 md:col-span-1 ">
+          <h5>Information</h5>
           <ul>
-            <li>
-              <LocationOn /> Add: 1234 Heaven Stress, Beverly Hill, Melbourne,
-              USA.
-            </li>
-            <li>
-              <Email /> Email: Contact@basictheme.com
-            </li>
-            <li>
-              <LocalPhone /> Phone Number: (800) 123 456 789
-            </li>
+            <li>About Us</li>
+            <li>Careers</li>
+            <li>Delivery Inforamtion</li>
+            <li>Privacy Policy</li>
+            <li>Terms & Condition</li>
           </ul>
         </div>
-      </div>
 
-      <div className="footer-col">
-        <h5>Information</h5>
-        <ul>
-          <li>About Us</li>
-          <li>Careers</li>
-          <li>Delivery Inforamtion</li>
-          <li>Privacy Policy</li>
-          <li>Terms & Condition</li>
-        </ul>
-      </div>
-
-      <div className="footer-col">
-        <h5>Customer Service</h5>
-        <ul>
-          <li>Shipping Policy</li>
-          <li>Help & Contact Us</li>
-          <li>Returns & Refunds</li>
-          <li>Online Stores</li>
-          <li>Terms & Conditions</li>
-        </ul>
+        <div className="p-5 col-span-2 md:col-span-1">
+          <h5>Customer Service</h5>
+          <ul>
+            <li>Shipping Policy</li>
+            <li>Help & Contact Us</li>
+            <li>Returns & Refunds</li>
+            <li>Online Stores</li>
+            <li>Terms & Conditions</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
