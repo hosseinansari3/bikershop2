7f5f3096c892e116a51de80a111dcede7c8fe29a
Hossein Ansari <36999119+hosseinansari3@users.noreply.github.com>
2023-07-03 11:51:00 +0330

    Update server.js

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ const path1 = path.join("__dirname", "../client/public/images");
 const dbUri = process.env.REACT_APP_DB_URI;
 
 const app = express();
-const port = 5000;
+const port = 3000;
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
