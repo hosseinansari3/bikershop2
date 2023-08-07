f3c2ac174f0566cb5d79595d5c6c1bb8578361bb
Hossein Ansari <36999119+hosseinansari3@users.noreply.github.com>
2023-08-07 10:38:21 +0330

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:5000";
+const baseUrl = "https://hosseinadev.ir";
 
 export const signIn = (email, password) =>
   axios.post(`${baseUrl}/users/signin`, { email, password });
